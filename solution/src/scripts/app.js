17be6382cc35faf6b53e8bb6a4c93a857ac4ee1a
Farhad <farhad@sent.com>
2016-07-31 23:56:15 +0200

    add VueJS

--- solution/src/scripts/app.js
+++ solution/src/scripts/app.js
@@ -1,3 +1,17 @@
-let world = "hello"
+import Vue from 'vue'
 
-console.log(world)
+Vue.config.delimiters = ['<%', '%>']
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        menuOn: true
+    },
+    methods: {
+        toggleMenu: function() {
+            menuOn.copy = !this.menuOn
+        }
+    }
+})
+
+console.log(app)
