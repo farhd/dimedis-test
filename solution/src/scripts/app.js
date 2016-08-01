b0fbcbed7b804b249841ed132e9cb0df6979e20d
Farhad <farhad@sent.com>
2016-08-01 09:28:27 +0200

    replace inline interpolation with bindings

--- solution/src/scripts/app.js
+++ solution/src/scripts/app.js
@@ -3,17 +3,21 @@ import Vue from 'vue'
 Vue.config.delimiters = ['<%', '%>']
 
 let app = new Vue({
+    // scope
     el: '#app',
+
+    // app state
     data: {
         menuOn: false,
         fruit: true,
         veggie: true,
         meat: true
     },
+
+    // handlers
     methods: {
         toggleMenu: function() {
             this.menuOn = !this.menuOn
-            console.log(this.menuOn)
         }
     }
 })
