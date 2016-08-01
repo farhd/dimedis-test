91d2d7c7b575e3a9557e01b4adf25013c0e60e24
Farhad <farhad@sent.com>
2016-08-01 09:15:05 +0200

    add filter handling

--- solution/src/scripts/app.js
+++ solution/src/scripts/app.js
@@ -5,7 +5,10 @@ Vue.config.delimiters = ['<%', '%>']
 let app = new Vue({
     el: '#app',
     data: {
-        menuOn: false
+        menuOn: false,
+        fruit: true,
+        veggie: true,
+        meat: true
     },
     methods: {
         toggleMenu: function() {
